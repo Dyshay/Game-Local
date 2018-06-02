56e59cfd62f44b78ac58b0952775b19e8e39ff68
Dylan Crelot <dyl.crel@gmail.com>
2018-06-03 00:11:03 +0200

    Modifiaction ViewModel
    Modification ViewModel Connect ainsi que vue
    Une modification du style à aussi été apporté au niveau du placeholder

--- Game/Game/ViewModels/HomeViewModel.cs
+++ Game/Game/ViewModels/HomeViewModel.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToolBox.Patterns.MVVM.Commands;
+using ToolBoxSupp.Mediator;
 
 namespace Game.ViewModels
 {
     public class HomeViewModel : NavigationPage
     {
+        // Binding bouton Connection
+        private ICommand _ConnectBtn;
+        public ICommand ConnectBtn
+        {
+            get { return _ConnectBtn ?? (_ConnectBtn = new RelayCommand(ConnectExec)); }
+        }
+
+        private void ConnectExec()
+        {
+            Mediator<NavigationPage>.Instance.Send(new ConnectViewModel());
+        }
     }
 }

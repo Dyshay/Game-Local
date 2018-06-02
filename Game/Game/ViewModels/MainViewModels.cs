c1892b55c68df349a0b9bf7a1355885d25568bf4
Dylan Crelot <dyl.crel@gmail.com>
2018-06-02 17:37:24 +0200

    Add ViewModel
    Ajouts ViewModel et style pour le placeholder

--- Game/Game/ViewModels/MainViewModels.cs
+++ Game/Game/ViewModels/MainViewModels.cs
@@ -4,11 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToolBox.Patterns.MVVM.Commands;
 
 namespace Game.ViewModels
 {
     public class MainViewModels : NavigationPage
     {
+        // Crée une ICommand qui va permettre le binding sur la vue
+        private ICommand _ConnectBtn;
+        public ICommand ConnectBtn
+        {
+            get { return _ConnectBtn ?? (_ConnectBtn = new RelayCommand(ConnectExec)); }
+        }
+
+        private void ConnectExec()
+        {
+            //Envoi via le médiateur le DataContext
+            Mediator<NavigationPage>.Instance.Send(new ConnectViewModel());
+        }
 
         // Constructeur s'abonnant à la méthode changement de ViewModel (Mediator)
         // Et Envoyant le ViewModel directement pour l'affichage de l'accueil

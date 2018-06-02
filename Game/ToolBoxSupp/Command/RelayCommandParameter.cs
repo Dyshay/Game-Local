3279e2dc79cdef702b3a077c5828b996a24e31e0
Dylan Crelot <dyl.crel@gmail.com>
2018-06-02 15:44:20 +0200

    Création ToolBox | MVVM

---
+++ Game/ToolBoxSupp/Command/RelayCommandParameter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBoxSupp.Command
+{
+    public class RelayCommandParameter : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly Action<object> _Execute;
+        private readonly Func<bool> _CanExecute;
+
+        public RelayCommandParameter(Action<object> Execute) : this(Execute, null)
+        {
+
+        }
+
+        public RelayCommandParameter(Action<object> Execute, Func<bool> CanExecute)
+        {
+            if (Execute == null)
+                throw new ArgumentNullException("Execute");
+
+            _Execute = Execute;
+            _CanExecute = CanExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return (_CanExecute == null) ? true : _CanExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _Execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Game/ToolBoxSupp/ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModelBase
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModelBase()
+        {
+            Type TViewModel = GetType();
+
+            IEnumerable<PropertyInfo> Commands = TViewModel.GetProperties().Where(PI => PI.PropertyType == typeof(ICommand) || PI.PropertyType.GetInterfaces().Contains(typeof(ICommand)));
+
+            foreach (PropertyInfo pi in Commands)
+            {
+                ICommand cmd = (ICommand)pi.GetMethod.Invoke(this, null);
+                PropertyChanged += (s, e) => cmd.RaiseCanExecuteChanged();
+            }
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName]string PropertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
